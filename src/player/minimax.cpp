deed346a6d42870f8700d05bd09fdec0f974ed7b
liuutin9 <136707519+liuutin9@users.noreply.github.com>
2023-06-18 14:27:07 +0800

    Update minimax.cpp

--- src/player/minimax.cpp
+++ src/player/minimax.cpp
@@ -43,7 +43,7 @@ void write_valid_spot(std::ofstream& fout) {
   int d = 5;
   while(true) {
     // Choose a random spot.
-    auto move = Minimax::get_move(root, d);
+    auto move = Minimax::get_move(root, 3);
     fout << move.first.first << " " << move.first.second << " "\
          << move.second.first << " " << move.second.second << std::endl;
     
